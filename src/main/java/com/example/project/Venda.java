794cb174fc15e10e910d388da8aa7b88154bcf41
Rodolpho Alves <rrpa15@live.com>
2020-10-11 21:15:23 -0300

    test e main

--- src/main/java/com/example/project/Venda.java
+++ src/main/java/com/example/project/Venda.java
@@ -1,5 +1,104 @@
 package com.example.project;
 
+import java.text.DateFormat;
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.Locale;
+
 public class Venda {
-    
+    private Loja loja;
+    private String datahora;
+    private int ccf;
+    private int coo;
+
+    public Venda(Loja loja, int ccf, int coo) {
+        this.loja = loja;
+        this.ccf = ccf;
+        this.coo = coo;
+        this.datahora = getDataAtual();
+    }
+
+    public Loja getLoja() {
+        return this.loja;
+    }
+
+    public void setLoja(Loja loja) {
+        this.loja = loja;
+    }
+
+    public String getDataHora() {
+        return this.datahora;
+    }
+
+    public void setDataHora() {
+        this.datahora = getDataAtual();
+    }
+
+    public void setDataHora(String datahora) {
+        this.datahora = datahora;
+    }
+
+    public static String getDataAtual() {
+        LocalTime hora = java.time.LocalTime.now();
+        String horaFormat = hora.toString()
+                                .substring(0,8);
+                                
+        Date d = new Date();
+        Locale brasil = new Locale("pt", "BR");
+
+        DateFormat data = DateFormat.getDateInstance(
+            DateFormat.SHORT,
+            brasil
+        );
+
+        String dataFormat = data.format(d);
+        return dataFormat + " " + horaFormat + "V";
+    }
+
+    public int getCcf() {
+        return this.ccf;
+    }
+
+    public void setCcf(int ccf) {
+        this.ccf = ccf;
+    }
+
+    public int getCoo() {
+        return this.coo;
+    }
+
+    public void setCoo(int coo) {
+        this.coo = coo;
+    }
+
+    public String dadosVenda() {
+
+        validarCamposObrigatorios();
+
+        String _ccf = " CCF: " + getCcf();
+        String _coo = " COO: " + getCoo();
+        return getDataHora() + _ccf + _coo;
+    }
+
+    public void validarCamposObrigatorios() {
+        if(getCcf() == 0) {
+            throw new RuntimeException("O Contador de Cupom Fiscal (CCF) é obrigatório.");
+        }
+
+        if(getCoo() == 0) {
+            throw new RuntimeException("O Contador de Ordem de Operação (COO) é obrigatório.");
+        }
+
+        if(String.format("%d",getCcf()).length() < 6) {
+            throw new RuntimeException("O CCF inserido não é válido.");
+        }
+
+        if(String.format("%d",getCoo()).length() < 6) {
+            throw new RuntimeException("O COO inserido não é válido.");
+        }
+
+        if(!(getLoja() instanceof Loja)) {
+            throw new RuntimeException("Loja é um campo obrigatório. Insira uma loja válida.");
+        }
+    }
 }
--- src/test/java/com/example/project/TestVenda.java
+++ src/test/java/com/example/project/TestVenda.java
@@ -5,8 +5,72 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class TestVenda {
-    	@Test
-	public void validarX() {
-        fail();
+    
+    private void verificarCampoObrigatorio(
+        String mensagemEsperada,
+        Venda venda
+    ) {
+        try {
+            venda.dadosVenda();
+            assertEquals("RuntimeException", "pass");
+        } catch (RuntimeException re) {
+            assertEquals(mensagemEsperada, re.getMessage());
+        }
+    }
+
+    String MSG_ERR_LOJA_INVALIDA = "Loja é um campo obrigatório. Insira uma loja válida.";
+    String MSG_ERR_CCF_INVALIDO = "O CCF inserido não é válido.";
+    String MSG_ERR_COO_INVALIDO = "O COO inserido não é válido.";
+    String MSG_ERR_CCF = "O Contador de Cupom Fiscal (CCF) é obrigatório.";
+    String MSG_ERR_COO = "O Contador de Ordem de Operação (COO) é obrigatório.";
+
+    private Loja lojaSample = new Loja();
+    private int COO = 123456;
+    private int CCF = 123456;
+
+    private Venda vendaSample = new Venda(
+        lojaSample,
+        CCF,
+        COO
+    );
+
+    @Test
+    public void validarLoja() {
+        Venda lojaNula = vendaSample;
+        lojaNula.setLoja(null);
+        verificarCampoObrigatorio(MSG_ERR_LOJA_INVALIDA, lojaNula);
+    }
+
+    @Test
+	public void validarCCF() {
+        Venda ccfVazio = vendaSample;
+        ccfVazio.setCcf(0);
+        verificarCampoObrigatorio(MSG_ERR_CCF, ccfVazio);
+
+        Venda ccfIncorreto = vendaSample;
+        ccfIncorreto.setCcf(12345);
+        verificarCampoObrigatorio(MSG_ERR_CCF_INVALIDO, ccfIncorreto);
+    }
+
+    @Test
+	public void validarCOO() {
+        Venda cooVazio = vendaSample;
+        cooVazio.setCoo(0);
+        verificarCampoObrigatorio(MSG_ERR_COO, cooVazio);
+
+        Venda cooIncorreto = vendaSample;
+        cooIncorreto.setCoo(12345);
+        verificarCampoObrigatorio(MSG_ERR_COO_INVALIDO, cooIncorreto);
+    }
+
+    @Test
+    public void validarDataHora() {
+        Venda venda = vendaSample;
+        String dataAtual = Venda.getDataAtual();
+        assertEquals(
+            dataAtual.substring(0, 15), 
+            venda.getDataHora()
+                 .substring(0, 15)
+        );
     }
 }
